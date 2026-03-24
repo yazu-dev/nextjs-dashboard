e17a2da951a3327dfa81513bb87af674a5a66194
yzau-dev <269179427+yazu-dev@users.noreply.github.com>
2026-03-24 22:45:08 +0800

    Fix error on deleting invoice

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -28,11 +28,9 @@ export function UpdateInvoice({ id }: { id: string }) {
 export function DeleteInvoice({ id }: { id: string }) {
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
   return (
-    <form action={deleteInvoiceWithId}>
-      <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
-        <span className="sr-only">Delete</span>
-        <TrashIcon className="w-5" />
-      </button>
-    </form>
+    <button onClick={deleteInvoiceWithId} className="rounded-md border p-2 hover:bg-gray-100">
+      <span className="sr-only">Delete</span>
+      <TrashIcon className="w-5" />
+    </button>
   );
 }
